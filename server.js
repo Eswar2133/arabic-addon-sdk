b709402a3f5910c6030c7f78234326945783f311
Eswar2133 <kuwait2412@gmail.com>
2025-07-05 15:20:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const http = require("http");
 const addonInterface = require("./index");
 
+console.log("addonInterface type:", typeof addonInterface);
+console.log("addonInterface keys:", Object.keys(addonInterface));
+
 http
   .createServer((req, res) => {
     if (req.url.endsWith(".json")) {
       res.setHeader("Access-Control-Allow-Origin", "*");
       res.setHeader("Content-Type", "application/json");
     }
-    // Call addonInterface as function (not .handler)
-    addonInterface(req, res);
+
+    if (typeof addonInterface === "function") {
+      addonInterface(req, res);
+    } else if (addonInterface && typeof addonInterface.handler === "function") {
+      addonInterface.handler(req, res);
+    } else {
+      res.statusCode = 500;
+      res.end("Addon interface not callable");
+      console.error("Addon interface is not callable");
+    }
   })
   .listen(process.env.PORT || 7000, () => {
     console.log("✅ Arabic addon is running");
