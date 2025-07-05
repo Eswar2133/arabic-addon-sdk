552c8f35b550b8990aad03e51ec0fdd5a0d1cb69
Eswar2133 <kuwait2412@gmail.com>
2025-07-05 15:12:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const MOVIES = [
     id: "arabic-movie-1",
     type: "movie",
     name: "الناصر صلاح الدين (1963)",
-    poster: "https://ia801600.us.archive.org/22/items/Nasser-Salah-El-Din/Nasser-Salah-El-Din.jpg",
+    poster:
+      "https://ia801600.us.archive.org/22/items/Nasser-Salah-El-Din/Nasser-Salah-El-Din.jpg",
     description: "فيلم مصري تاريخي من إخراج يوسف شاهين.",
   },
 ];
@@ -40,4 +41,5 @@ builder.defineStreamHandler(({ id }, cb) => {
   }
 });
 
+// Export the handler function directly
 module.exports = builder.getInterface();
